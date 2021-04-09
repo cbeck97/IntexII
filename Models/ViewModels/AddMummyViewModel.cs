f0032d9ff51f6d4e0a2d03f11b13f8270634e39e
Carter Beck <46453763+cbeck97@users.noreply.github.com>
2021-04-08 20:34:31 -0600

    Changed permissions and got add burial functional

---
+++ Models/ViewModels/AddMummyViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BYUFagElGamous1_5.Models.ViewModels
+{
+    public class AddMummyViewModel
+    {
+        public Mummy Mummy { get; set; }
+        public Location Location { get; set; }
+    }
+}
