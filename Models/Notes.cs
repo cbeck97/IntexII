6c406d66f66b9bf089824b9cbf6a71e2d775dd4a
Carter Beck <46453763+cbeck97@users.noreply.github.com>
2021-04-10 10:33:43 -0600

    Added NoteTitle Field

--- Models/Notes.cs
+++ Models/Notes.cs
@@ -16,5 +16,6 @@ namespace BYUFagElGamous1_5.Models
         public int? MummyId { get; set; }
         public string Category { get; set; }
         public string Text { get; set; }
+        public string NoteTitle { get; set; }
     }
 }
